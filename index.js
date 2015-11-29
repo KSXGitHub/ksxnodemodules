b559549286b2d7c04c18a6e11dcabf9a089a02f0
KSXGitHub <hvksmr1996@gmail.com>
2015-11-29 12:12:38 +0700

    index.js: add all usable modules

--- index.js
+++ index.js
@@ -2,14 +2,22 @@
 function KSXNodeModules() {
 	'use strict';
 	
-	var utilities = require('./lib/utilities.js');
-	
 	var self = this;
 	
-	utilities
-		.setConst
-			(self, "utilities", utilities)
-	;
+	add("utilities", "js");
+	add("mimex", "json");
+	add("mimeutils", "js");
+	add("stringutils", "js");
+	add("prime", "js");
+	
+	function add(name, ext) {
+		Object.defineProperty(self, name, {
+			value: require("./lib/" + name + '.' + ext),
+			enumarable: true,
+			writable: false,
+			configurable: false
+		});
+	}
 	
 }
 
